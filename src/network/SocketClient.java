496fb18d65e6a92f41c060a5950bc6819758ec9a
sakuyui <405544641@qq.com>
2019-06-26 20:40:34 +0800

    socket

--- src/network/SocketClient.java
+++ src/network/SocketClient.java
@@ -1,10 +1,67 @@
 package network;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
 import java.net.*;
-public class SocketClient{
+import java.io.*;
 
+
+/**
+ * @author Michael Huang
+ *
+ */
+public class ChatClient {
+    Socket s = null;
+    DataOutputStream dos = null;
+    DataInputStream dis = null;
+    private boolean bConnected = false;
+
+
+    Thread tRecv = new Thread(new RecvThread());
+
+    public void connect(String ip,int port) {
+        try {
+            s = new Socket(ip, port);
+            dos = new DataOutputStream(s.getOutputStream());
+            dis = new DataInputStream(s.getInputStream());
+            System.out.println("~~~~~~~~连接成功~~~~~~~~!");
+            bConnected = true;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void disconnect() {
+        try {
+            dos.close();
+            dis.close();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    private class RecvThread implements Runnable {
+
+        public void run() {
+            try {
+                while (bConnected) {
+                    //连接成功
+                    String str = dis.readUTF();
+                    //taContent.setText(taContent.getText() + str + '\n');
+                }
+            } catch (SocketException e) {
+                System.out.println("退出了，bye!");
+            } catch (EOFException e) {
+                System.out.println("退出了，bye!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
 }
