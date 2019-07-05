866580c8d9895bd87cb362561b06fba03e2acb26
sakuyui <405544641@qq.com>
2019-07-05 20:50:04 +0800

    insert

--- src/automaton/methods/InsertMethod.java
+++ src/automaton/methods/InsertMethod.java
@@ -3,10 +3,17 @@ package automaton.methods;
 import automaton.INodeFunc;
 import automaton.InfCollection;
 import automaton.SQLSession;
+import dbms.RelationRow;
+import dbms.TableWriter;
 import dbms.logic.DatabaseDBMSObj;
+import dbms.logic.TableDBMSObj;
 import dbms.logic.TableStructure;
+import dbms.logic.TableStructureItem;
 import filesystem.PropertiesFileTool;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InsertMethod implements INodeFunc {
     @Override
@@ -30,10 +37,58 @@ public class InsertMethod implements INodeFunc {
         }
 
 
-        //检验参数对应性
-        if(infCollection.columNames.size()==0){  //直接用values的情况 (必须插入全部值)
 
+        try{
+            TableDBMSObj tableDBMSObj = new TableDBMSObj(tableName,new DatabaseDBMSObj(sqlSession.curUseDatabase, DatabaseDBMSObj.rootPath));
+//insert into student values(111,222)
+        //检验参数对应性
+            if(infCollection.columNames.size()==0){  //直接用values的情况 (必须插入全部值)
+                for (TableStructureItem tableStructureItem:tableDBMSObj.tableStructure.dts){
+                    infCollection.columNames.push(tableStructureItem.conlumName);
+                }
+            }
+
+            TableStructure tableStructure=tableDBMSObj.tableStructure;
+            //检测参数个数是否匹配
+            if(infCollection.columNames.size() != infCollection.others.size()){
+                System.out.println("Values not match the table column's count");
+                return null;
+            }else{
+                //write
+                TableWriter tableWriter = new TableWriter();
+
+                RelationRow relationRow = new RelationRow(tableStructure);
+                int size = tableStructure.dts.size();
+                for (int i=0;i<size;i++){
+                    String val= infCollection.others.pop();
+                    String cName=infCollection.columNames.pop();
+                    switch (tableStructure.dts.get(size-i-1).type){
+                        case INT32:
+                            if(!relationRow.setVal(cName,Integer.parseInt(val))){
+                                System.out.println("Insert Error.(Column name '"+cName+"'not exist)");
+                                return null;
+                            }
+                            break;
+                        case STRING:
+                            if(!relationRow.setVal(cName,val)){
+                                System.out.println("Insert Error.(Column name '"+cName+"' not exist)");
+                                return null;
+                            }
+                            break;
+
+                    }
+                }
+                List<RelationRow> relationRows=new ArrayList<>();
+                relationRows.add(relationRow);
+                tableWriter.appendRelations(relationRows,tableDBMSObj);
+                System.out.println("Insert successful.");
+                return null;
+            }
+        }catch (Exception e){
+            System.out.println("Insert Fails:Data type not match");
+            //e.printStackTrace();
         }
+
         return null;
     }
 }
