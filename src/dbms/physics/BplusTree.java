e20cf6fca5ce66e26f94beb4048bfc38e8c16a05
unknown <1713411181@qq.com>
2019-07-06 22:35:32 +0800

    b+

---
+++ src/dbms/physics/BplusTree.java
@@ -0,0 +1,79 @@
+package dbms.physics;
+
+public class BplusTree<K extends Comparable<K>> {
+    //
+    protected BplusTreeNode<K> root;
+    //
+    protected BplusTreeNode<K> head;
+    //
+    protected int order;
+    //
+    protected int height;
+    //
+    public BplusTree(int order){
+        if(order < 3){
+            System.out.println("order must greater than 2");
+            System.exit(0);
+        }
+        this.order = order;
+        this.height = 0;
+        this.root = new BplusTreeNode<K>(true,true);
+        this.head = this.root;
+    }
+    //
+    public BplusTree(){
+        this.order = 3;
+        this.height = 0;
+        this.root = new BplusTreeNode<K>(true,true);
+        this.head = this.root;
+    }
+    //
+    public int getHeight() {
+        return height;
+    }
+    //
+    public int getOrder() {
+        return order;
+    }
+
+    public BplusTreeNode<K> getHead() {
+        return head;
+    }
+
+    public BplusTreeNode<K> getRoot() {
+        return root;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setHead(BplusTreeNode<K> head) {
+        this.head = head;
+    }
+
+    public void setRoot(BplusTreeNode<K> root) {
+        this.root = root;
+    }
+
+    public void insert(K key, Object value){
+        root.insertOrUpdate(key, value, this);
+//        System.out.println(root.toString());
+    }
+
+    public Object remove(K key){
+        return root.remove(key, this);
+    }
+
+    public Object get(K key){
+        return root.get(key);
+    }
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/dbms/physics/Test.java
@@ -0,0 +1,61 @@
+package dbms.physics;
+
+public class Test {
+
+    public static void main(String[] args){
+        boolean random = false;
+        int order = 10;
+        int size = 1000000;
+        if(random){
+            int d = 2;
+            System.out.println(Math.ceil(d/2.0));
+        }
+        else {
+            BplusTree<Integer> myTree = new BplusTree<Integer>(order);
+            System.out.println(order + "叉B+树，顺序插入测试：size:"+ size +".");
+            long start = System.currentTimeMillis();
+            for(int i = 0; i < size; i++)
+                myTree.insert(i, "数字" + Integer.toString(i));
+            long end = System.currentTimeMillis() - start;
+            System.out.println("花费时间：" + end);
+            System.out.println(order + "叉B+树，顺序查找测试：size:"+ size +".");
+            start = System.currentTimeMillis();
+//            BplusTreeNode<Integer> aa = myTree.getHead();
+//            while (aa!=null){
+//                System.out.println(aa.toString());
+//                aa = aa.getNext();
+//            }
+            String ss = null;
+            int count = 0;
+            for(int j = 0; j < size; j++){
+                ss = (String)myTree.get(j);
+                if(ss == null){
+                    System.out.println("找不到啊弟弟！");
+                    System.exit(0);
+                }
+                else{
+                    count++;
+                }
+            }
+            end = System.currentTimeMillis() - start;
+            System.out.println("花费时间：" + end);
+            System.out.println(count);
+            System.out.println(order + "叉B+树，顺序删除测试：size:"+ size +".");
+            start = System.currentTimeMillis();
+            count = 0;
+            for(int j = 0; j < size; j++){
+                ss = (String)myTree.remove(j);
+                if(ss == null){
+                    System.out.println("找不到啊弟弟！");
+                    System.exit(0);
+                }
+                else{
+                    count++;
+                }
+            }
+            end = System.currentTimeMillis() - start;
+            System.out.println("花费时间：" + end);
+            System.out.println(count);
+        }
+    }
+}
